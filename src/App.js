a7a1757b155f07b3c803bae098987ad16780ec4e
Ng Wei En <weien1292@gmail.com>
2021-07-22 00:48:33 +0800

    Implement Pathways modal (#7)
    This commit also contains a whole bunch of other random changes that I was too lazy to isolate.
    
    Co-authored-by: Raziq <amirulraziq@pop-os.localdomain>

--- src/App.js
+++ src/App.js
@@ -1,57 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./components/header";
 import ProfileCard from "./components/profile-card";
-// import ProfileModal from "./components/profile-modal";
+import ProfileModal from "./components/profile-modal";
 import pathways from "./pathways";
 
 import "./App.css";
 
-// const setHash = (hash) => window.history.replaceState({}, "", `#${hash}`);
+const setHash = (hash) => window.history.replaceState({}, "", `#${hash}`);
 
 function App() {
-  /*
   const [isModalOpen, setIsModalOpen] = useState(false);
-
   const [activePathwayId, setActivePathwayId] = useState("");
+
   const activateModal = (pathwayId) => {
     setActivePathwayId(pathwayId);
     setIsModalOpen(true);
   };
-  */
+
+  useEffect(() => {
+    // Checks hash and ensures that any modal with a corresponding name is open.
+    const ensureModalFromHash = () => {
+      const pathwayId = window.location.hash.slice(1);
+      if (pathways.hasOwnProperty(pathwayId)) {
+        if (!isModalOpen || pathwayId !== activePathwayId) {
+          activateModal(pathwayId);
+          return true;
+        }
+      }
+    };
+
+    // If modal is open, ensure that the hash is active.
+    if (isModalOpen) {
+      setHash(activePathwayId);
+    } else {
+      // If modal is not open and `activeMentorId === ""`, this must be the
+      // initial load. Check for a hash, and open the modal if such an ID
+      // exists.
+      if (activePathwayId === "") {
+        if (!ensureModalFromHash()) {
+          // Otherwise, set a default ID, but do not open the modal.
+          setActivePathwayId("");
+        }
+      } else {
+        // An ID exists, but the modal is not open, so remove the hash.
+        setHash("");
+      }
+    }
+
+    // Add event listeners to catch if the user manually changes the hash.
+    window.addEventListener("hashchange", ensureModalFromHash, false);
+    return () =>
+      window.removeEventListener("hashchange", ensureModalFromHash, false);
+  }, [isModalOpen, activePathwayId]);
 
   return (
     <div className="container">
       <Header />
 
       <div className="canvas">
-        <p className="disclaimer">
-          <small>
-            The privacy and safety of our mentors is of utmost priority to
-            Advisory. Any attempt to approach or contact our mentors outside of
-            the parameters of the Advisory Mentorship Programme—whilst claiming
-            affiliation to Advisory, or misrepresenting a relationship to
-            Advisory—will be treated as misrepresentation, even fraudulent
-            misrepresentation, as considered under the Misrepresentation Act.
-            Advisory will take legal action against any individuals or
-            organisations who attempt to deceive, harass, or otherwise request
-            dishonest assistance from our mentors.
-          </small>
-        </p>
         <div className="card-container">
           {Object.entries(pathways).map(([key, pathway]) => (
             <ProfileCard
               key={key}
               pathwayDetails={pathway}
-              onReadMore={() => {}}
+              onReadMore={() => activateModal(key)}
             />
           ))}
         </div>
-        {/* <ProfileModal
+        <ProfileModal
           isOpen={isModalOpen}
-          pathway={pathwayDetails[activePathwayId]}
+          pathway={pathways[activePathwayId]}
           onClose={() => setIsModalOpen(false)}
-        /> */}
+        />
       </div>
     </div>
   );
--- src/pathways.js
+++ src/pathways.js
@@ -6,8 +6,13 @@ const pathways = {
       {
         name: "Philip Hur",
         designation: "Head of APAC Partnerships, Commerce & Signals",
+        imageUrl: "/instructors/PhilipHur.jpg",
       },
-      { name: "Jimmy Guo", designation: "Partner Solutions Manager" },
+      {
+        name: "Jimmy Guo",
+        designation: "Partner Solutions Manager",
+        imageUrl: "/instructors/JimmyGuo.jpg",
+      },
     ],
     blurb:
       "As companies look to scale, many layers of thinking are applied to effect better decision making. In this pathway, we will cover foundational business strategy, explore some key strategic frameworks, and go through real-life case studies where these frameworks can be applied. At the end of this Pathway, you will learn strategic tools, concepts and perspectives that will allow you to understand and apply effective strategic thinking at a company level.",
@@ -16,9 +21,21 @@ const pathways = {
     title: "Career Development",
     imageUrl: "/pathways/CareerDevelopment.jpg",
     instructors: [
-      { name: "Kush Sagar", designation: "Commerce Platforms Lead, APAC" },
-      { name: "Cecilia", designation: "tbc" },
-      { name: "Nayna", designation: "tbc" },
+      {
+        name: "Kush Sagar",
+        designation: "Commerce Platforms Lead, APAC",
+        imageUrl: "/instructors/KushSagar.jpg",
+      },
+      {
+        name: "Cecilia LV",
+        designation: "Partner Manager APAC, Creative Platforms",
+        imageUrl: "/instructors/CeciliaLV.jpg",
+      },
+      {
+        name: "Nayana Kamath",
+        designation: "Partner Solutions Manager",
+        imageUrl: "/instructors/NayanaKamath.jpg",
+      },
     ],
     blurb:
       "Students often face difficulties in navigating the transition between school and work. You might find it difficult to envision how the subjects or courses you learn in school can directly translate into your future career. In this pathway, we will provide you with a mechanism to identify potential career options, based on your individual skills and accomplishments, and teach you what we wish we had known at the start of our careers. Join us to discover how to map your experiences to different job opportunities and take a step towards owning your career journey.",
@@ -30,11 +47,17 @@ const pathways = {
       {
         name: "Anthony Isaakidis",
         designation: "Manager, Technical Partner Management",
+        imageUrl: "/instructors/AnthonyIsaakidis.jpg",
       },
-      { name: "Vinh Mai", designation: "Measurement Partnerships Lead, APAC" },
+      {
+        name: "Vinh Mai",
+        designation: "Measurement Partnerships Lead, APAC",
+        imageUrl: "/instructors/VinhMai.jpg",
+      },
       {
         name: "Naomi Peles",
         designation: "Partner Solutions Lead, Australia & New Zealand",
+        imageUrl: "/instructors/NaomiPeles.jpg",
       },
     ],
     blurb:
@@ -44,7 +67,11 @@ const pathways = {
     title: "Computational Thinking",
     imageUrl: "/pathways/ComputationalThinking.jpg",
     instructors: [
-      { name: "Rishi Daryanani", designation: "Solutions Engineer" },
+      {
+        name: "Rishi Daryanani",
+        designation: "Solutions Engineer",
+        imageUrl: "/instructors/RishiDaryanani.jpg",
+      },
     ],
     blurb:
       "Coding forms the backbone of the technology around us, and behind coding lies concepts. In this pathway, you will learn about certain foundational concepts of computing and how these relate to the thought process behind scale in companies. In addition we will explore some basics in Python and data structures, and in managing tech interviews with greater ease. At the end of this Pathway, you will take away an entry point to break into the tech industry, based on my own personal career journey, including my sharing on bridging interests between the business world and engineering.",
