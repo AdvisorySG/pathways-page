e8798f9b14446839499258847a01dc489fa49a82
amirulraziqi <66960654+amirulraziqi@users.noreply.github.com>
2021-07-26 19:34:31 +0800

    Moved disclaimer to Footer (#9)
    Co-authored-by: Raziq <amirulraziq@pop-os.localdomain>

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -66,19 +66,6 @@ const Header = () => (
           </a>{" "}
           to view the application guide on CVs and resumes.
         </p>
-        <p>
-          <small>
-            The privacy and safety of our faculty is of utmost priority to
-            Advisory. Any attempt to approach or contact our faculty outside of
-            the parameters of Pathways With Facebook—whilst claiming affiliation
-            to Advisory, or misrepresenting a relationship to Advisory—will be
-            treated as misrepresentation, even fraudulent misrepresentation, as
-            considered under the Misrepresentation Act. Advisory will take legal
-            action against any individuals or organisations who attempt to
-            deceive, harass, or otherwise request dishonest assistance from our
-            faculty.
-          </small>
-        </p>
       </div>
     </div>
   </div>
